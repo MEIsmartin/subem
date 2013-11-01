016324040536d69f23b2d035f22f745e3e221268
Shaun <smartin@meiresearch.org>
2013-11-01 09:41:12 -0500

    Things set up, page 64.

--- lib/subscribem/engine.rb
+++ lib/subscribem/engine.rb
@@ -9,6 +9,14 @@ module Subscribem
       g.test_framework :rspec, :view_specs => false
     end
 
+    config.to_prepare do
+      root = Subscribem::Engine.root
+      extenders_path = root + "app/extenders/**/*.rb"
+      Dir.glob(extenders_path) do |file|
+        Rails.configuration.cache_classes ? require(file) : load(file)
+      end
+    end
+
     initializer 'subscribem.middleware.warden' do
       Rails.application.config.middleware.use Warden::Manager do |manager|
         manager.default_strategies :password
